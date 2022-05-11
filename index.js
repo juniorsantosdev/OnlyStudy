e2a8ee23c9e3508aeeaae576aa687d5eef281f23
Junior Santos <josejunior.engtec@gmail.com>
2022-05-11 11:17:54 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let mes = "Fevereiro";
+
+switch (mes){
+    case "Janeiro":
+        console.log(mes);
+        break;
+    
+    case "Fevereiro":
+        console.log(mes);    
+        break;
+
+    case "Março":
+        console.log(mes);
+        break;
+    
+    default:
+        console.log(mes);
+}
+// vai imprimir "Fevereiro"
+
+
+
